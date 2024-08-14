49f2ff044a4a2c1a9e073053351bf4497ce17bed
anithashantini <113708572+anithashantini@users.noreply.github.com>
2024-08-14 14:33:42 +0530

    Add files via upload

---
+++ Diamond_pattern.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    int rows, i, j;
+    printf("Enter the number of rows for the diamond: ");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; i++) {
+        for (j = rows; j > i; j--) {
+            printf(" ");
+        }
+        for (j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (i = rows - 1; i >= 1; i--) {
+        for (j = rows; j > i; j--) {
+            printf(" ");
+        }
+        for (j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
+        
+    }
+    return 0;
+}
